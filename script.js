19f0302a88c9156790903100e38c4717a2955777
Cemil Kerimoglu <cemil.a.kerimoglu@gmail.com>
2022-07-14 18:06:43 +0200

    Created the task item with delete and edit buttons

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 // ola's section 
-const myButton = document.querySelector('.add');
+const addTaskButton = document.querySelector('.btn-primary');
 const myList = document.querySelector('.new-list');
 const myInput = document.querySelector('#new');
 
 const myArray = [];
-myButton.addEventListener('click', () => {
-    const newItem = document.createElement("li");
 
+addTaskButton.addEventListener('click', () => {
+    let newItem = document.createElement('li');
     newItem.innerText = `${myInput.value}`;
-    myArray.push(newItem);
-    console.log(myArray);
-    newItem.setAttribute('class', 'list-item');
-    myList.appendChild(newItem);
-});
+    let newDivision = document.createElement('div');
 
-//myList.addEventListener('click', (e) => {
-//    if (e.target.classList.contains('list-item')) logString();
-//})
-// Cemil's section
-// Sujana's section
+    let deleteButton = document.createElement('button');
+    deleteButton.innerText = 'Delete';
+    let editButton = document.createElement('button');
+    editButton.innerText = 'Edit';
+
+    newDivision.appendChild(deleteButton);
+    newDivision.appendChild(editButton);
+
+    newItem.appendChild(newDivision);
+    myArray.push(newItem);
+    myList.appendChild(newItem);
+})
+
+
+
+
+
+
+
