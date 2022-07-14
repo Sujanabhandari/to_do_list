6441b711571bc8d5e19616f4cdccb6495f1a44fd
Olaoluwa Lewis <olewis4@web.de>
2022-07-14 16:22:30 +0200

    input and add button

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 // ola's section 
+const myButton = document.querySelector('.add');
+const myList = document.querySelector('.new-list');
+const myInput = document.querySelector('#new');
+
+const myArray = [];
+myButton.addEventListener('click', () => {
+    const newItem = document.createElement("li");
+
+    newItem.innerText = `${myInput.value}`;
+    myArray.push(newItem);
+    console.log(myArray);
+    newItem.setAttribute('class', 'list-item');
+    myList.appendChild(newItem);
+});
+
+//myList.addEventListener('click', (e) => {
+//    if (e.target.classList.contains('list-item')) logString();
+//})
 // Cemil's section
 // Sujana's section
